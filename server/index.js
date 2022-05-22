d06cfd1d6d65756d3ab990cc35cae32385ef0508
Shailendra Kumar <shailendrakumar@Shailendras-MacBook-Pro.local>
2022-05-23 03:36:50 +0530

    Moved user routes inside user.js file in contoller

--- server/index.js
+++ server/index.js
@@ -16,19 +16,7 @@ app.get('/', function (req, res) {
     return res.send("Hi");
 });
 
-app.get("/user/:userId/posts",(req,res)=>{
-    //Returns all the post created by the user themselves.
-    Post.find({'user':req.params.userId}).populate({ path: 'user', select: 'username firstName'})
-        .exec((err, posts) => {
-            if (err) {
-                console.log(err);
-                return res.send(err);
-            }
-            return res.send(posts)
-        })
-})
-
-
+app.use('/user',require('./Controller/user'));
 
 app.post('/post/add', function (req, res) {
 
@@ -67,62 +55,6 @@ app.post('/comment/add', function (req, res) {
         })
 
 })
-
-
-app.post('/user/signIn', function (req, res) {
-    console.log(req.body);
-    User.findOne({
-        username: req.body.username,
-        password: req.body.password
-    }, function (err, user) {
-        if (err) {
-            console.log("Error in Signing In...", err);
-            res.redirect('back');
-        }
-        if (user) {
-            console.log(user);
-            res.cookie(user._id, user.username, { httpOnly: true });
-            res.json(
-                {
-                    data: user,
-                    statusCode: 200
-                }
-            );
-        }
-        else {
-            console.log("Invalid Username or Password");
-            res.json(
-                {
-                    statusCode: 401
-                });
-        }
-    })
-});
-
-app.post('/user/signUp', function (req, res) {
-
-    console.log(req.body);
-
-    User.findOne({ username: req.body.username }, function (err, user) {
-        if (err) {
-            console.log("Error in Signing Up...", err);
-            res.redirect('back');
-        }
-        if (user) {
-            console.log("User already Exist...");
-            return res.json({
-                statusCode: 401
-            })
-        }
-        else {
-            User.create(req.body);
-            return res.json({
-                statusCode: 200
-            })
-
-        }
-    })
-});
 
 app.use(fileUpload() );
 app.post('/post/image/add',(req,res)=>{
@@ -206,38 +138,6 @@ app.get('/delete/post/:id', function (req, res) {
             res.send(err);
         })
 
-})
-
-app.get('/user/get', (req, res) => {
-
-    User.find().then((users) => {
-        res.send(users);
-    }).catch(err => { console.log("Error in fetchig users,", err) })
-
-})
-
-
-
-
-app.get('/user/:id',(req,res)=>{
-    User.findById(req.params.id).then((user)=>{
-        res.send(user)
-    }).catch((err)=>{
-        console.log("Error in fetchig users,", err)
-    })
-
-})
-
-app.patch('/user/:id',(req,res)=>{
-    User.findById(req.params.id).then((user)=>{
-        for(key in req.body){
-            user[key]=req.body[key];
-        }
-        user.save();
-        res.send("Updated")
-    }).catch((err)=>{
-        console.log("Error in fetchig users,", err)
-    })
 })
 
 
